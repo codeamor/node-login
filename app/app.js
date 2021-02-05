4186cff462846e704d9c9eb843ca4d989d2cae01
codeamor <ssm2319@gmail.com>
2021-02-05 17:41:09 +0900

    feat: front-end js file created

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// 모듈
 const express = require("express");
 const app = express();
 
@@ -9,6 +10,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);
 
