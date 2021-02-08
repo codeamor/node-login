85b000c5c49662a1c73ada5332b268308475166b
codeamor <ssm2319@gmail.com>
2021-02-08 17:03:37 +0900

    feat: 서버 응답데이터 처리

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,9 +10,27 @@ const output = {
   },
 };
 
+const users = {
+  id: ["손수민", "김수민", "박수민"],
+  psword: ["1234", "12345", "123456"],
+};
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({ success: true });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하였습니다.",
+    });
   },
 };
 
